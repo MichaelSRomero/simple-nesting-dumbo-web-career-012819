26e07aea5f4a04d28570c3ea80cb3d549ba6ef9d
Michael Romero <liikemike@aim.com>
2018-12-31 02:50:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}}.merge(programmer_hash)
 end
 
 def changing_alan
